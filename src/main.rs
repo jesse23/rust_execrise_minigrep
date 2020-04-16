ced80296a338ec4152fabe9b14b81d3ad08592f1
pengw <wenjia.peng@siemens.com>
2020-04-16 23:09:20 +0000

    add unit test

--- src/main.rs
+++ src/main.rs
@@ -25,13 +25,44 @@ fn process_input( args: Vec<String> ) -> GrepInput {
         filename: String::from("")
     };
 
-    if args.len() > 0 {
+    if args.len() > 1 {
         input.query = args[1].clone();
     }
 
-    if args.len() > 1 {
-        input.query = args[2].clone();
+    if args.len() > 2 {
+        input.filename = args[2].clone();
     }
 
     return input;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn process_input_filename() {
+        let input = process_input(vec![ String::from("cmd"), String::from("query_str"), String::from("file.txt") ]);
+        assert_eq!( input.filename, "file.txt" );
+    }
+
+    #[test]
+    fn process_input_query() {
+        let input = process_input(vec![ String::from("cmd"), String::from("query_str"), String::from("file.txt") ]);
+        assert_eq!( input.query, "query_str" );
+    }
+
+    #[test]
+    fn process_input_cmd_only() {
+        let input = process_input(vec![ String::from("cmd") ]);
+        assert_eq!( input.query, "" );
+        assert_eq!( input.filename, "" );
+    }
+
+    #[test]
+    fn process_input_query_str_only() {
+        let input = process_input(vec![ String::from("cmd"), String::from("query_str") ]);
+        assert_eq!( input.query, "query_str" );
+        assert_eq!( input.filename, "" );
+    }
 }
