1b2424533b8c5c493005a8919b8d2030e61884df
pengw <wenjia.peng@siemens.com>
2020-04-16 21:40:48 +0000

    impl for process_input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::fs;
+use std::env;
+
+struct GrepInput {
+    query: String,
+    filename: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = process_input( env::args().collect() );
+
+    println!("Searching for {}", input.query);
+    println!("In file {}", input.filename);
+
+    let contents = fs::read_to_string(input.filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+
+}
+
+fn process_input( args: Vec<String> ) -> GrepInput {
+    let mut input = GrepInput{
+        query: String::from(""),
+        filename: String::from("")
+    };
+
+    if args.len() > 0 {
+        input.query = args[1].clone();
+    }
+
+    if args.len() > 1 {
+        input.query = args[2].clone();
+    }
+
+    return input;
 }
