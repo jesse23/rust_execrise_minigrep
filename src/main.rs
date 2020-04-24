82af699f8d4fdd87026a9b249ce453cc8b30562e
pengw <wenjia.peng@siemens.com>
2020-04-24 21:00:08 +0000

    unit test

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,53 @@
-use std::fs;
 use std::env;
+use std::fs;
 
-struct GrepInput {
+struct Config {
     query: String,
     filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+#[cfg(test)]
+mod test_config_object {
+    use super::*;
+
+    #[test]
+    fn config_param_exceed() {
+        let args = vec![String::from("cmd")];
+        assert_eq!(Config::new(&args).err(), Some("not enough arguments"));
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // let ( query, filename ) = parse_config(&args);
-    let input = process_input( &args );
+    let input = parse_config(&args);
 
     println!("Searching for {}", input.query);
     println!("In file {}", input.filename);
 
-    let contents = fs::read_to_string(input.filename)
-        .expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(input.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
-
 }
 
-fn parse_config( args: &[String] ) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-
-    (query, filename)
-}
-
-fn process_input( args: &[String] ) -> GrepInput {
-    let mut input = GrepInput{
+fn parse_config(args: &[String]) -> Config {
+    let mut input = Config {
         query: String::from(""),
-        filename: String::from("")
+        filename: String::from(""),
     };
 
     if args.len() > 1 {
@@ -46,36 +62,44 @@ fn process_input( args: &[String] ) -> GrepInput {
 }
 
 #[cfg(test)]
-mod tests {
+mod test_parse_config {
     use super::*;
 
     #[test]
-    fn process_input_filename() {
-        let args = vec![ String::from("cmd"), String::from("query_str"), String::from("file.txt") ];
-        let input = process_input(&args);
-        assert_eq!( input.filename, "file.txt" );
+    fn parse_config_filename() {
+        let args = vec![
+            String::from("cmd"),
+            String::from("query_str"),
+            String::from("file.txt"),
+        ];
+        let input = parse_config(&args);
+        assert_eq!(input.filename, "file.txt");
     }
 
     #[test]
-    fn process_input_query() {
-        let args = vec![ String::from("cmd"), String::from("query_str"), String::from("file.txt") ];
-        let input = process_input(&args);
-        assert_eq!( input.query, "query_str" );
+    fn parse_config_query() {
+        let args = vec![
+            String::from("cmd"),
+            String::from("query_str"),
+            String::from("file.txt"),
+        ];
+        let input = parse_config(&args);
+        assert_eq!(input.query, "query_str");
     }
 
     #[test]
-    fn process_input_cmd_only() {
-        let args = vec![ String::from("cmd") ];
-        let input = process_input(&args);
-        assert_eq!( input.query, "" );
-        assert_eq!( input.filename, "" );
+    fn parse_config_cmd_only() {
+        let args = vec![String::from("cmd")];
+        let input = parse_config(&args);
+        assert_eq!(input.query, "");
+        assert_eq!(input.filename, "");
     }
 
     #[test]
-    fn process_input_query_str_only() {
-        let args = vec![ String::from("cmd"), String::from("query_str") ];
-        let input = process_input(&args);
-        assert_eq!( input.query, "query_str" );
-        assert_eq!( input.filename, "" );
+    fn parse_config_query_str_only() {
+        let args = vec![String::from("cmd"), String::from("query_str")];
+        let input = parse_config(&args);
+        assert_eq!(input.query, "query_str");
+        assert_eq!(input.filename, "");
     }
 }
