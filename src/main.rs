b428dcda83e28a673ff2a1a653cad205d593ed5d
pengw <wenjia.peng@siemens.com>
2020-04-25 00:27:33 +0000

    refactor lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,8 @@
+// https://github.com/rust-lang/rls-vscode/issues/513
+use minigrep::run;
+use minigrep::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-#[cfg(test)]
-mod test_config_object {
-    use super::*;
-
-    #[test]
-    fn config_param_exceed() {
-        let args = vec![String::from("cmd")];
-        assert_eq!(Config::new(&args).err(), Some("not enough arguments"));
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -47,12 +19,4 @@ fn main() {
 
         process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
